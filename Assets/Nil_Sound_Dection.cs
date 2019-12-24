50136837ad2a08ea234340a22d7e2c069c3e7a9d
Unknown <ns1228yt@gmail.com>
2019-12-24 00:17:33 +0000

    Basic Sound Detection

---
+++ Assets/Nil_Sound_Dection.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityStandardAssets.Characters.ThirdPerson
+{
+    public class Nil_Sound_Dection : MonoBehaviour
+    {
+        public GameObject AI;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void OnTriggerEnter(Collider coll)
+        {
+            if (coll.tag == "Player")
+            {
+                AI.GetComponent<Nil_BasicAI>().target = coll.gameObject;
+                if (AI.GetComponent<Nil_BasicAI>().target.GetComponent<Nil_Sound_Maker>().isRunning == true)
+                {
+                    AI.GetComponent<Nil_BasicAI>().state = Nil_BasicAI.State.SEARCH;
+                    
+                }
+                else
+                {
+                    AI.GetComponent<Nil_BasicAI>().state = Nil_BasicAI.State.PATROL;
+                }
+            }
+        }
+    }
+}
